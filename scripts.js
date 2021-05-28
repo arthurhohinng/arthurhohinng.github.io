1bede53b3a034863dd11aafb496ed55cafd9f876
Arthur Ng <artrurng@gmail.com>
2021-05-28 16:53:33 -0400

    Updated resume

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 function hide(){
-  document.getElementById("main").style.display = "none";
-  document.getElementById("about").style.display = "none";
+  document.querySelector("#main").style.display = "none";
+  document.querySelector("#about").style.display = "none";
 }
 
 function openSidebar(){
-  document.getElementById("sidebar").classList.toggle('active');
-  document.getElementById("main").classList.toggle('active');
-  document.getElementById("about").classList.toggle('active');
+  document.querySelector("#sidebar").classList.toggle('active');
+  document.querySelector("#main").classList.toggle('active');
+  document.querySelector("#about").classList.toggle('active');
 }
 
 function showHome(){
   hide();
-  document.getElementById("main").style.display = "block";
+  document.querySelector("#main").style.display = "block";
 }
 
 function showAbout(){
   hide();
-  document.getElementById("about").style.display = "block";
+  document.querySelector("#about").style.display = "block";
 }
 
